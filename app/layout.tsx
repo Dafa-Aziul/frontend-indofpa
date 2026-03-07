356217d59f8f3e03f31bf3757a9b03d1951e95b2
Dafa-Aziul <dafaaziul625@gmail.com>
2026-03-07 15:26:28 +0700

    ds

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'IndoFPA',
     description: 'Analisis kuesioner jadi lebih mudah.',
-    images: ['/og-image.webp'],
+    images: ['./public/images/og-image.webp'],
   },
 };
 
